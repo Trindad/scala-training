004546980c706ad567f50e9ddd2fa34c0572c4b8
Silvana Trindade <syletri@gmail.com>
2017-04-28 12:50:14 -0300

    Fatorial

---
+++ factorial.scala
@@ -0,0 +1,12 @@
+object factorial {
+
+    def factorial(x : BigInt): BigInt = {
+        if (x == 0) 1 else x * factorial (x - 1)
+    }
+
+    def main(args: Array[String]) = 
+    {
+        println(factorial(30))
+    }
+
+}
